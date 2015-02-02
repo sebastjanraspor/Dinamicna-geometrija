3a84aa404cfb303e4118016a43553630b20bd3c2
MitjaR <rozman.mitj@gmail.com>
2015-02-02 11:26:55 +0100

    Razdeljen na dve datoteki. Dodana PDF.
    Dodana moznost pretvorbe podatkov iz platna v PDF format. Preiskuseno na premicah in tockah. Vmestnik ponudi urejanje, kjer lahko uporabnik recimo izbrise kroznice, ki jih ne zeli imeti izrisane v PDF.

--- novejsiPopravki/uporabniskiVmestnik.hs
+++ novejsiPopravki/uporabniskiVmestnik.hs
@@ -2,138 +2,13 @@ import HTk.Toplevel.HTk
 import HTk.Toolkit.MarkupText as MarkupText
 import Circle
 import StrukturniObjekti
-
-osnovniPretvornik1 :: Distance -> Float
-osnovniPretvornik1 a = read(show(a))
-
-osnovniPretvornik2 ::  Float -> Distance
-osnovniPretvornik2 a = read(show(a))
-
--- osnovniPretvornik3 :: Distance -> Double
--- osnovniPretvornik3 a = read(show(a))
-
-osnovniPretvornik4 :: Double -> Distance
-osnovniPretvornik4 a = read(show(a))
-
-pretvoriIzStandardneOblike :: OsnovniObjekt Double -> (Distance,Distance,Distance,Distance)
-pretvoriIzStandardneOblike (Kroznica (x,y) r) =(x0a,y0a,x1a,y1a)
-    where
-        x0fl=x-r
-        y0fl=y-r
-        x1fl=x+r
-        y1fl=y+r
-        x0a=(osnovniPretvornik4 x0fl) ::Distance
-        y0a=(osnovniPretvornik4 y0fl) ::Distance
-        x1a=(osnovniPretvornik4 x1fl) ::Distance
-        y1a=(osnovniPretvornik4 y1fl) ::Distance
-        
-
-pretvorbaOmejenaRadialna x0 y0 x1 y1 = (xk1,xk2,yk1,yk2)
-    where
-        x0fl=osnovniPretvornik1 x0 ::Float
-        y0fl=osnovniPretvornik1 y0 ::Float
-        x1fl=osnovniPretvornik1 x1 ::Float
-        y1fl=osnovniPretvornik1 y1 ::Float
-        r=sqrt((x1fl-x0fl)^2+(y1fl-y0fl)^2)
-        (xs,ys)=(x0fl,y0fl)
-        xk1=osnovniPretvornik2 (xs-r)
-        xk2=osnovniPretvornik2 (xs+r)
-        yk1=osnovniPretvornik2 (ys-r)
-        yk2=osnovniPretvornik2 (ys+r)
-
-standardizirajObliko :: [Char] -> Distance -> Distance -> Distance -> Distance -> (OsnovniObjekt Double)
---standardizirajObliko nizObjekta x0 y0 x1 y1        
-standardizirajObliko "Kroznica" x0 y0 x1 y1 = osObjekt
-    where
-        x0fl=(osnovniPretvornik1 x0) ::Float
-        y0fl=(osnovniPretvornik1 y0) ::Float
-        x1fl=(osnovniPretvornik1 x1) ::Float
-        y1fl=(osnovniPretvornik1 y1) ::Float
-        r=sqrt((x1fl-x0fl)^2+(y1fl-y0fl)^2)
-        (xs,ys)=(x0fl,y0fl)
-
-        niz = " Kroznica "++show((xs,ys))++" "++show(r)
-        osObjekt = (read niz):: (OsnovniObjekt Double)
-        -- Tole zgoraj je NAPAKA HASKLLA ce namesto niz damo kar vsebuje ne dela!!!!!
-        
-standardizirajObliko "Premica" x0 y0 x1 y1 = (read niz):: (OsnovniObjekt Double)
-    where
-        x0fl=(osnovniPretvornik1 x0) ::Float
-        y0fl=(osnovniPretvornik1 y0) ::Float
-        x1fl=(osnovniPretvornik1 x1) ::Float
-        y1fl=(osnovniPretvornik1 y1) ::Float
-        niz = " Premica "++show((x0fl,y0fl))++" "++show((x1fl,y1fl))
-        
-standardizirajObliko "Tocka" x0 y0 _ _ = (read niz):: (OsnovniObjekt Double)
-    where
-        x0fl=(osnovniPretvornik1 x0) ::Float
-        y0fl=(osnovniPretvornik1 y0) ::Float
-        niz = " Tocka "++show((x0fl,y0fl))
-        
-        
--- super ugotovitev: Kasnejsega parsa ne motijo presledki derive (Read)
-sestaviBesediloElementa :: [Char] -> Distance -> Distance -> Distance -> Distance -> Distance -> [Char]
-sestaviBesediloElementa "Kroznica" x0 y0 x1 y1 zaporednaSt = razsirjeniNizObKor
-    where
-        x0fl=(osnovniPretvornik1 x0) ::Float
-        y0fl=(osnovniPretvornik1 y0) ::Float
-        x1fl=(osnovniPretvornik1 x1) ::Float
-        y1fl=(osnovniPretvornik1 y1) ::Float
-        r=sqrt((x1fl-x0fl)^2+(y1fl-y0fl)^2)
-        (xs,ys)=(x0fl,y0fl)
-        razsirjeniNizObKor = "[K"++show(zaporednaSt)++"; Kroznica "++show((xs,ys))++" "++show(r)++"];//;"
-        
-sestaviBesediloElementa "Premica" x0 y0 x1 y1 zaporednaSt = razsirjeniNizObKor
-    where
-        x0fl=osnovniPretvornik1 x0 ::Float
-        y0fl=osnovniPretvornik1 y0 ::Float
-        x1fl=osnovniPretvornik1 x1 ::Float
-        y1fl=osnovniPretvornik1 y1 ::Float
-        razsirjeniNizObKor = "[P"++show(zaporednaSt)++"; Premica "++show((x0fl,y0fl))++" "++show((x1fl,y1fl))++"];//;"
-        
-sestaviBesediloElementa "Daljica" x0 y0 x1 y1 zaporednaSt = razsirjeniNizObKor
-    where
-        x0fl=osnovniPretvornik1 x0 ::Float
-        y0fl=osnovniPretvornik1 y0 ::Float
-        x1fl=osnovniPretvornik1 x1 ::Float
-        y1fl=osnovniPretvornik1 y1 ::Float
-        razsirjeniNizObKor = "[D"++show(zaporednaSt)++"; Daljica "++show((x0fl,y0fl))++" "++show((x1fl,y1fl))++"];//;"
-
-sestaviBesediloElementa "Tocka" x0 y0 _ _ zaporednaSt = razsirjeniNizObKor
-    where
-        x0fl=osnovniPretvornik1 x0 ::Float
-        y0fl=osnovniPretvornik1 y0 ::Float
-        razsirjeniNizObKor = "[T"++show(zaporednaSt)++"; Tocka "++show((x0fl,y0fl))++"];//;"
-        
-        
-
-spremembaObjektaP :: [Char] -> Distance -> Distance -> Distance -> Distance -> [(Distance,Distance)]
-spremembaObjektaP "Kroznica" x0 y0 x1 y1 = [(xk1,yk1),(xk2,yk2)]
-    where
-        (xk1,xk2,yk1,yk2) = pretvorbaOmejenaRadialna x0 y0 x1 y1
-
-spremembaObjektaP "Daljica" x0 y0 x1 y1 = [(x0,y0),(x1,y1)]
-spremembaObjektaP "Premica" x0 y0 x1 y1 = [(xk1,yk1),(xk2,yk2)]
-    where
-        x0fl=osnovniPretvornik1 x0 ::Float
-        y0fl=osnovniPretvornik1 y0 ::Float
-        x1fl=osnovniPretvornik1 x1 ::Float
-        y1fl=osnovniPretvornik1 y1 ::Float
-        sprX=x0fl-x1fl
-        sprY=y0fl-y1fl
-        k=sprY/sprX
-        sirinaPlatna=1000
-        xk1=osnovniPretvornik2 (x0fl-sirinaPlatna)
-        xk2=osnovniPretvornik2 (x0fl+sirinaPlatna)
-        yk1=osnovniPretvornik2 (y0fl-k*sirinaPlatna)
-        yk2=osnovniPretvornik2 (y0fl+k*sirinaPlatna)
-
-spremembaObjekta :: (CanvasItem b) =>[Char] -> Distance -> Distance -> Distance -> Distance -> Config b
-spremembaObjekta niz x0 y0 x1 y1 =coord (spremembaObjektaP niz x0 y0 x1 y1)
-
-
+import PretvorbePodatkov
+import NarediPDF
+-- import HTk.Kernel.GridPackOptions
 
 novMain=main
+
+--oknoLat::IO()
 
 main::IO()
 main =
@@ -142,10 +17,10 @@ main =
 
     oo00 <- newFrame main []
     grid oo00 [GridPos (1,1),GridPadX 1,GridPadY 1]	
-	
+    
     oo02 <- newFrame oo00 []
     grid oo02 [GridPos (2,1),GridPadX 1,GridPadY 1]
-	
+    	
     -- ni ista enota pri size
     urejevalnikBesedila <- newEditor oo02 [size (80,25)]
     grid urejevalnikBesedila [GridPos (1,1),GridPadX 1,GridPadY 1]
@@ -155,6 +30,62 @@ main =
     
     vhodZaRelacije <- (newEntry oo02 [value "", width 80])::IO (Entry String) 
     grid vhodZaRelacije [GridPos (1,3),GridPadX 1,GridPadY 1]
+    
+    
+    let oknoLat =
+            do
+            oknoLat <- newFrame oo00 []
+            grid oknoLat [GridPos (2,1),GridPadX 1,GridPadY 1, Sticky NSEW]
+        
+            besedilo <- (getValue urejevalnikBesedila) :: IO String
+            
+            besediloRezultat <- (getValue urejevalnikBesedilaRezultat) :: IO String
+        
+            urejevalnikLat <- newEditor oknoLat [size (80, 40)]
+            grid urejevalnikLat [GridPos (1,1),GridPadX 1,GridPadY 1]
+        
+            let besediloLat::[MarkupText]
+                besediloLat = sezMarkTeksta
+                    where
+                        --da stran prvo in jo zamenja
+                        (x:sez1) = (lines besedilo)
+                        (y:sez2) = (lines besediloRezultat)
+                        sez3 = ("Besedilo za urejanje:":sez1)++("*":"Izracunani podatki:":sez2)
+                        sezMarkTeksta = concat [[prose x, newline]|x <- sez3]
+                        
+            urejevalnikLat # new besediloLat
+        
+            ooL2 <- newFrame oknoLat []
+            grid ooL2 [GridPos (1,2),GridPadX 1,GridPadY 1]
+        
+            gumbL1 <- newButton ooL2 [text "Ovrednoti"]
+            gumbL2 <- newButton ooL2 [text "Zapri urejanje"]
+        
+            klikgL1 <- clicked gumbL1
+            klikgL2 <- clicked gumbL2
+        
+            let sezG = [gumbL1, gumbL2]
+            -- Naslednja funkcija delayWish je pri hitrosti danasnjih racunalnikov mogoce odvec (vse gumbe da gor na enkrat)
+            delayWish $ mapM_ (\(gumb,(x,y)) -> grid gumb [GridPos (x,y),GridPadX 1,GridPadY 1]) (zip sezG [(x,1)|x <-[1..100]])
+            
+            
+            let ovrednotiLatNaSpremembah =
+                    do
+                    besediloSprememb <- (getValue urejevalnikLat) :: IO String
+                    let [besediloSpremembe, besediloRezultatSpremembe] = split '*' besediloSprememb
+                    narediLatDatIzVsega besediloSpremembe besediloRezultatSpremembe
+            
+            _ <- spawnEvent(forever (
+                        (
+                        klikgL1 >>> ovrednotiLatNaSpremembah
+                        )
+                        +>
+                        (klikgL2 >>> destroy oknoLat)
+                        )
+                        )
+                        
+            done
+    
     
     oo01 <- newFrame oo00 []
     grid oo01 [GridPos (1,1),GridPadX 1,GridPadY 1]	
@@ -176,9 +107,9 @@ main =
     gumb4 <- newButton oo3 [text "Premica"]
     gumb5 <- newButton oo3 [text "Kroznica"]
     gumb6 <- newButton oo3 [text "Daljica"]
-    gumb7 <- newButton oo3 [text "Preberi vse"]
-    gumb8 <- newButton oo3 [text "Nedolocen objekt"]
-    gumb9 <- newButton oo3 [text "Razresi relacije"]
+    gumb7 <- newButton oo3 [text "Naredi PDF"]
+    gumb8 <- newButton oo3 [text "Izpisuj kordinate"]
+    --gumb9 <- newButton oo3 [text "Razresi relacije"]
     
     klikg1 <- clicked gumb1
     klikg2 <- clicked gumb2
@@ -188,9 +119,9 @@ main =
     klikg6 <- clicked gumb6
     klikg7 <- clicked gumb7
     klikg8 <- clicked gumb8
-    klikg9 <- clicked gumb9
+    --klikg9 <- clicked gumb9
 
-    let sezG = [gumb1,gumb8,gumb3,gumb4,gumb5,gumb6,gumb7,gumb2,gumb9]
+    let sezG = [gumb1,gumb8,gumb3,gumb4,gumb5,gumb6,gumb7,gumb2]
     -- Naslednja funkcija delayWish je pri hitrosti danasnjih racunalnikov mogoce odvec (vse gumbe da gor na enkrat)
     delayWish $ mapM_ (\(gumb,(x,y)) -> grid gumb [GridPos (x,y),GridPadX 1,GridPadY 1]) (zip sezG [(x,1)|x <-[1..100]])
     
@@ -231,16 +162,39 @@ main =
             
     urejevalnikBesedilaRezultat # new besediloVmestnikaRezultat
     
-    let narisiObjekt (Kroznica (x,y) r) = createOval platno [coord[(x1,y1),(x2,y2)], outline "grey"]
-            where 
-                (x1,y1,x2,y2) = pretvoriIzStandardneOblike (Kroznica (x,y) r)
-
-        narisiObjekt (Tocka (x1,y1)) = createOval platno [coord[(x1a-1,y1a-1),(x1a+1,y1a+1)], outline "grey"]
-            where
-                x1a=(osnovniPretvornik4 x1) ::Distance
-                y1a=(osnovniPretvornik4 y1) ::Distance
+    -- let narisiObjekt (Kroznica (x,y) r) = (
+                                        -- do
+                                        -- objektNOV <- createOval platno [coord[(x1,y1),(x2,y2)], outline "grey"]
+                                        -- objektNOV
+                                        
+                                        -- oznakaP <- createCanvasTag platno []
+                                        -- addCanvasTag (withTag objektNOV) oznakaP
+                                        -- return oznakaP
+                                        -- )
+            -- where 
+                -- (x1,y1,x2,y2) = pretvoriIzStandardneOblike (Kroznica (x,y) r)
                 
-        -- narisiObjekt (Premica (x1,y1) (x2,y2)) = createLine platno [coord[(x1a,y1a),(x2a,y2a)]]
+
+        -- narisiObjekt (Tocka (x1,y1)) = (
+                                        -- do
+                                        -- objektNOV <- createOval platno [coord[(x1a-1,y1a-1),(x1a+1,y1a+1)], outline "grey", filling "red"]
+                                        -- objektNOV
+                                        -- oznakaP <- createCanvasTag platno []
+                                        -- addCanvasTag (withTag objektNOV) oznakaP
+                                        -- return oznakaP
+                                        -- )
+            -- where
+                -- x1a=(osnovniPretvornik4 x1) ::Distance
+                -- y1a=(osnovniPretvornik4 y1) ::Distance
+                
+        -- narisiObjekt (Premica (x1,y1) (x2,y2)) = (
+                                        -- do
+                                        -- objektNOV <- createLine platno [coord[(x1a,y1a),(x2a,y2a)], outline "grey"]
+                                        -- objektNOV
+                                        -- oznakaP <- createCanvasTag platno []
+                                        -- addCanvasTag (withTag objektNOV) oznakaP
+                                        -- return oznakaP
+                                        -- )
             -- where
                 -- x1a=(osnovniPretvornik4 x1) ::Distance
                 -- y1a=(osnovniPretvornik4 y1) ::Distance
@@ -252,8 +206,20 @@ main =
     (odKlikMiskaM1, _) <- bindSimple platno (ButtonRelease (Just 1))
     (navadniPremik, _) <- bind platno [WishEvent [] Motion]
     
+     
+    let besediloOdvisnoOdObjekta oznakaP osObjekt =[ --sezOznakP = [
+            action (
+            do
+            oznakaP # coord [(x0a,y0a), (x1a,y1a)]    
+            done
+            )
+            [link (show osObjekt)]
+            ,newline
+            ]
+            where
+                (x0a,y0a,x1a,y1a) = (pretvoriIzStandardneOblike osObjekt)
 
-    
+            
     let klikMiske :: [Char] -> Event () -- to se mi zdi zamudno(dolgovezno) delati z vzorci (patern match) se da hitreje?
         klikMiske "Kroznica" = (
             do
@@ -335,7 +301,7 @@ main =
             (
             do
             (x, y) <- premikPoKlikM1 >>>= \i-> return (x i, y i)
-            always (gumb1 # text ("(x,y): " ++ show (x,y) ++ "  (x0,y0): " ++ show (x0,y0)))
+            always (gumb8 # text ("(x,y): " ++ show (x,y) ++ "  (x0,y0): " ++ show (x0,y0)))
             --let (xk1,xk2,yk1,yk2) = pretvorbaOmejenaRadialna x0 y0 x y
             --let [(xk1,yk1),(xk2,yk2)] = spremembaObjektaP "Kroznica" x0 y0 x y
             always(oznakaP # (spremembaObjekta nizObjekta x0 y0 x1 y1))
@@ -350,6 +316,9 @@ main =
         odKlikMiske :: Distance -> Distance -> Distance -> Distance -> [Char] -> CanvasTag -> Event ()
         odKlikMiske x0 y0 x1 y1 nizObjekta oznakaP = (
             do
+            -- iskaze se da je tako bolj tocno (move je diskretna funkcija)
+            always(oznakaP # (spremembaObjekta nizObjekta x0 y0 x1 y1))
+            
             steviloObjektov <- always(
                     do
                     nizStevilaObjektov <- (getValue zaklenjenIzhod) :: IO String
@@ -365,6 +334,9 @@ main =
                 do
                 nizOmejitev <- (getValue vhodZaRelacije) :: IO String
                 besedilo <- (getValue urejevalnikBesedila) :: IO String
+                
+                oznakaP # (spremembaObjekta nizObjekta x0 y0 x1 y1)
+                -- zato da vedno lahko vidimo zacetni priblizek (ob kliku se vedno ponastavi)
                 done
                 putStrLn nizOmejitev
                 putStrLn besedilo
@@ -372,9 +344,14 @@ main =
                 let sezObjektov=poisciUstrezneKroge nizOmejitev besedilo (standardizirajObliko nizObjekta x0 y0 x1 y1)
                 putStrLn (show (sezObjektov))
                 
-                mapM_ narisiObjekt sezObjektov
+                -- mapM_ narisiObjekt sezObjektov
+                -- mapM narisiObjektKroznica sezObjektov
+                -- let sezOznakP = mapM narisiObjekt sezObjektov
 
-                let sez =[prose "Tukaj se izpisejo rezultati izracunov:", newline] ++ [prose (unlines (map show (sezObjektov)))]
+                let sez =[prose "Tukaj se izpisejo rezultati izracunov:", newline] ++ concat (map (\z -> besediloOdvisnoOdObjekta oznakaP z) sezObjektov)
+                -- ++ concat (map (\z -> besediloOdvisnoOdObjekta oznakaP z sezOznakP) sezObjektov)
+                
+                --[prose (unlines (map show (sezObjektov)))]
                 let besediloVmestnikaRezultat = sez
 
                 do
@@ -407,7 +384,7 @@ main =
     let popravljajKoordinate = (
             do
             (x, y) <- navadniPremik >>>= \i-> return (x i, y i)
-            always (gumb1 # text ("(x,y): " ++ show (x,y)))
+            always (gumb8 # text ("(x,y): " ++ show (x,y)))
             done
             )    
     
@@ -416,6 +393,10 @@ main =
 						do
 						always(novMain)
 						always(destroy main)
+                        )
+                        +>
+                        (
+                        klikg7 >>> oknoLat
                         )
                         +>
                         (klikg2 >>> destroy main)
@@ -442,7 +423,7 @@ main =
                         -- (
                         -- do
                         -- (x, y) <- navadniPremik >>>= \i-> return (x i, y i)
-                        -- always (gumb1 # text ("(x,y): " ++ show (x,y)))
+                        -- always (gumb8 # text ("(x,y): " ++ show (x,y)))
                         -- done
                         -- )
                             )
